026587f9b089f58566f936db4fc24e89bad1e6f8
HardenYan <hardenyan@pin-genie.com>
2019-12-20 19:44:03 +0800

    发短信测试

--- src/main/java/cc/mrbird/common/util/SendMsgsUtils.java
+++ src/main/java/cc/mrbird/common/util/SendMsgsUtils.java
@@ -9,10 +9,24 @@ import org.apache.commons.httpclient.params.HttpMethodParams;
 
 
 import java.net.URLEncoder;
+import java.util.ArrayList;
 import java.util.List;
 
 public class SendMsgsUtils {
 
+
+//    public static void main(String[] args) throws Exception {
+//        SendSmsIn sendSmsIn =new SendSmsIn();
+//        sendSmsIn.setAccesskey("qAWK2aKDAzQzq0rJ");
+//        sendSmsIn.setAccessSecret("o24oHc3n54JHb4ipmjrdwT5SwEQ2I7hF");
+//        sendSmsIn.setSign("32712");
+//        sendSmsIn.setSendUrl("http://api.1cloudsp.com/api/v2/single_send");
+//        sendSmsIn.setSendContent("晏展辉##520##52035656");
+//        List<String> stringList =new ArrayList<>();
+//        stringList.add("15675311927");
+//        sendSmsIn.setMobiles(stringList);
+//        sendsms(sendSmsIn);
+//    }
 
 
 
@@ -32,7 +46,7 @@ public class SendMsgsUtils {
                 new NameValuePair("accesskey", smsIn.getAccesskey()),
                 new NameValuePair("secret",smsIn.getAccessSecret()),
                 new NameValuePair("sign", smsIn.getSign()),
-               // new NameValuePair("templateId", "100"),
+               new NameValuePair("templateId", "50249"),
                 new NameValuePair("mobile", mobileString),
                 new NameValuePair("content", URLEncoder.encode(smsIn.getSendContent(), "utf-8"))//（示例模板：{1}您好，您的订单于{2}已通过{3}发货，运单号{4}）
         };
